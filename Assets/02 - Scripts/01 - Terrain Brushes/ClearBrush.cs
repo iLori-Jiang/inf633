738ddb49d3d3f584dd0d5eb67e9e3ab07ccf6e8e
iLori <1349307719@qq.com>
2023-10-10 21:19:05 +0200

    add brushes

---
+++ Assets/02 - Scripts/01 - Terrain Brushes/ClearBrush.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearBrush : TerrainBrush
+{
+
+    public override void draw(int x, int z)
+    {
+        for (int zi = -radius; zi <= radius; zi++)
+        {
+            for (int xi = -radius; xi <= radius; xi++)
+            {
+                if (Mathf.Pow(xi, 2) + Mathf.Pow(zi, 2) <= Mathf.Pow(radius, 2))
+                {
+                    terrain.set(x + xi, z + zi, 0);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/02 - Scripts/01 - Terrain Brushes/PerlinBrush.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinBrush : TerrainBrush
+{
+
+    public float height = 5f;
+    private float factor = 3;
+    public float perlinScale = 10f;
+
+    public override void draw(int x, int z)
+    {
+        for (int zi = -radius; zi <= radius; zi++)
+        {
+            for (int xi = -radius; xi <= radius; xi++)
+            {
+                float temp_height = terrain.get(x + xi, z + zi);
+                float dist = Mathf.Sqrt(Mathf.Pow(xi, 2) + Mathf.Pow(zi, 2));
+                // gaussian coeff
+                float param = dist / (radius / factor);
+                float alpha = Mathf.Exp(-Mathf.Pow(param, 2) / 2) / ((radius / factor) * Mathf.Sqrt(2 * Mathf.PI));
+
+                // perlin coeff
+                float perlin_1 = Mathf.PerlinNoise((x + xi) / perlinScale, (z + zi) / perlinScale);
+                float perlin_2 = Mathf.PerlinNoise((x + xi) / perlinScale / 2, (z + zi) / perlinScale * 2);
+
+                terrain.set(x + xi, z + zi, temp_height + alpha * (1 + perlin_1 - perlin_2) * height);
+            }
+        }
+    }
+}
